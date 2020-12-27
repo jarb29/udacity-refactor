7cdecfca359a82181bb6e4d133e0fcf8962efc77
jarb29 <jarb29@gmail.com>
2020-12-26 21:50:35 -0300

    type: updating body: adding git ignore

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
+export POSTGRES_USERNAME=a;lex
+export POSTGRES_PASSWORD=alessss
 export POSTGRES_HOST=
 export POSTGRES_DB=
 export AWS_BUCKET=
